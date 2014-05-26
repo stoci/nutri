cfc2e0c099a5ccf4e4cdd1c3be1df481168d37fc
stoci <jablitz@ncsu.edu>
2014-05-26 08:36:15 -0400

    add food to mysql database operational,updated Food class for possible future ORM

--- src/com/food/Food.java
+++ src/com/food/Food.java
@@ -1,68 +1,98 @@
 package src.com.food;
 
-public class Food implements java.io.Serializable
+public class Food
 {
-	private double carbs = 0.0;		//grams of carbohydrates
-	private double protein = 0.0;	//grams of protein
-	private double fat = 0.0;		//grams of fat
-	//private double quantity = 0.0;	//grams per serving
-	private String name = "";		//name of food
+	/**/
+	private String name = "";	//name of food
+	private int carbs = 0;		//grams of carbohydrates
+	private int protein = 0;	//grams of protein
+	private int fat = 0;		//grams of fat
+	private int serv_size = 0;	//grams per serving or quantity of item (typically 1)
+	private int cholesterol = 0;//milligrams cholesterol
+	private int sodium = 0;		//milligrams sodium
 	
-	//constructor for initializing instance variables
-	public Food(String n, double c, double p, double f,double q)
+	/*constructor for properly formatted fields*/
+	public Food(String name, int carbs, int protein, int fat, int serv_size,
+			int cholesterol, int sodium)
 	{
-		this.name=n;
-		this.carbs=c;
-		this.protein=p;
-		this.fat=f;
-		//this.setQuantity(q);
+		super();
+		this.name = name;
+		this.carbs = carbs;
+		this.protein = protein;
+		this.fat = fat;
+		this.serv_size = serv_size;
+		this.cholesterol = cholesterol;
+		this.sodium = sodium;
 	}
-	public Food(String [] item)
+	
+	/*constructor for String inputs*/
+	public Food(String...item)
 	{
 		this.name=item[0];
-		this.carbs=Double.parseDouble(item[1]);
-		this.protein=Double.parseDouble(item[2]);
-		this.fat=Double.parseDouble(item[3]);
-		//this.setQuantity(Double.parseDouble(item[4]));
+		this.serv_size=Integer.parseInt(item[1]);
+		this.fat=Integer.parseInt(item[2]);
+		this.cholesterol=Integer.parseInt(item[3]);
+		this.sodium=Integer.parseInt(item[4]);
+		this.carbs=Integer.parseInt(item[1]);
+		this.protein=Integer.parseInt(item[2]);
+		
+	}
+
+	/*commence getter/setters*/
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getCarbs() {
+		return carbs;
+	}
+
+	public void setCarbs(int carbs) {
+		this.carbs = carbs;
+	}
+
+	public int getProtein() {
+		return protein;
+	}
+
+	public void setProtein(int protein) {
+		this.protein = protein;
+	}
+
+	public int getFat() {
+		return fat;
+	}
+
+	public void setFat(int fat) {
+		this.fat = fat;
+	}
+
+	public int getServ_size() {
+		return serv_size;
+	}
+
+	public void setServ_size(int serv_size) {
+		this.serv_size = serv_size;
+	}
+
+	public int getCholesterol() {
+		return cholesterol;
+	}
+
+	public void setCholesterol(int cholesterol) {
+		this.cholesterol = cholesterol;
+	}
+
+	public int getSodium() {
+		return sodium;
+	}
+
+	public void setSodium(int sodium) {
+		this.sodium = sodium;
 	}
 	
-	public double getCarbs()
-	{
-		return this.carbs;
-	}
-	public double getProtein()
-	{
-		return this.protein;
-	}
-	public double getFat()
-	{
-		return this.fat;
-	}
-	public String getName()
-	{
-		return this.name;
-	}
-	public void setCarbs(double c)
-	{
-		this.carbs=c;
-	}
-	public void setProtein(double p)
-	{
-		this.protein=p;
-	}
-	public void setFat(double f)
-	{
-		this.fat=f;
-	}
-	public void setName(String n)
-	{
-		this.name=n;
-	}
-	/*
-	public double getQuantity() {
-		return quantity;
-	}
-	public void setQuantity(double q) {
-		this.quantity = q;
-	}*/
 }
--- src/com/main/NutriTrack.java
+++ src/com/main/NutriTrack.java
@@ -34,91 +34,6 @@ public class NutriTrack extends Application
 	static public void main(String [] args) throws Exception
 	{
 		launch(args);
-		System.out.println("NutriTrack v0.1 Developed by stoci");
-		//load serialized DBs from files into program -- eventually use MySQL
-		NutriTrack.initializeFoodDB();
-		NutriTrack.initializeCalendarDB();
-		Connection conn = null; Statement s = null; ResultSet rs = null;
-		
-		/*processes commands in infinite loop*/
-		while(true)
-		{
-			System.out.print(">>> ");
-			BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-			String input = bufferedReader.readLine();
-			//String input = System.console().readLine();
-			String [] commands = input.trim().split(" ");
-			
-			/*exit command breaks while loop*/
-			if(input.trim().equalsIgnoreCase("exit")) break;
-			/*prints out all food currently in database file*/
-			else if(commands[0].equalsIgnoreCase("print") && commands[1].equalsIgnoreCase("food"))
-			{
-				printFoodDB();
-			}
-			/*print out all meals in a specific day -- input format: xx/xx/xxxx */
-			else if(commands[0].equalsIgnoreCase("print") && commands[1].equalsIgnoreCase("day") 
-			&& commands[2].length()==10)
-			{
-				Day d = calendar_db.get(commands[2]);
-				if(d!=null)
-					printDay(d);
-				else
-					System.out.println("That date doesn't exist.");
-			}
-			/*print out every day including meals in database*/
-			else if(commands[0].equalsIgnoreCase("print") && commands[1].equalsIgnoreCase("day") 
-			&& commands[2].equalsIgnoreCase("all"))
-			{
-				printDayAll();
-			}
-			/*add new food to database*/
-			else if(commands[0].equalsIgnoreCase("addf")&& commands[1]!=null
-			&&commands[2]!=null && commands[3]!=null && commands[4]!=null)
-			{
-				food_db.put(commands[1],new Food(commands[1],Double.parseDouble(commands[2]),
-				Double.parseDouble(commands[3]),Double.parseDouble(commands[4]),Double.parseDouble(commands[5])));
-			}
-			/*add specific food[3] to specific date(xx/xx/xxxx)[1] and specific meal(lunch)[2]*/
-			else if(commands[0].equalsIgnoreCase("addd")&& commands[1].length()==10
-			&&commands[2]!=null && commands[3]!=null)
-			{
-				//check to see if name of food in db
-				Food f = food_db.get(commands[3]);
-				if(f!=null)
-				{
-					//check to see if day already in calendar_db
-					Day d = calendar_db.get(commands[1]);
-					if(d!=null)
-					{
-						System.out.println("Adding to calendar_db");
-						addFood2Day(f,d,commands[2]);
-					}
-					//date not in calendar_db
-					else
-					{
-						Day d1 = new Day();
-						d1.setDate(commands[1]);
-						//add date to LinkedHashMap calendar_db
-						calendar_db.put(commands[1],d1);
-						addFood2Day(f,calendar_db.get(commands[1]),commands[2]);
-					}
-				}
-				//food not in database
-				else System.out.println("Please use addf command to add this food!");
-			}
-			/*print out list of valid commands*/
-			else if(commands[0].equalsIgnoreCase("help"))
-			{
-				System.out.println(" print food\n addf [name_of_food carbs protein fat grams/serving]\n addd [date meal name_of_food]\n print day [date]\n print day all\n exit");
-			}
-			/*command doesn't exist*/
-			else
-				System.out.println("Unrecognized command. Type help to see valid commands.");
-		}
-		/*update database files with any changes*/
-		updateCalendarDB();
-		updateFoodDB();
 		
 	}
 	
@@ -161,9 +76,9 @@ public class NutriTrack extends Application
 		System.out.printf("%-25s %10s %10s %10s %n", "Name", "C", "P", "F");
 		for ( Map.Entry<String,Food> entry : food_db.entrySet() )
 		{
-			System.out.printf("%-25s %10.2f %10.2f %10.2f %n", entry.getKey(), 
-			entry.getValue().getCarbs(), entry.getValue().getProtein(),
-			entry.getValue().getFat());
+			//System.out.printf("%-25s %10.2f %10.2f %10.2f %n", entry.getKey(), 
+			//entry.getValue().getCarbs(), entry.getValue().getProtein(),
+			//entry.getValue().getFat());
 		}
 	}
 	/*print the specified Day object including meals to console*/
@@ -181,35 +96,35 @@ public class NutriTrack extends Application
 		System.out.printf("%-15s","Breakfast: ");
 		while(litr.hasNext())
 		{
-			System.out.print(litr.next().getName()+" ");
+		//	System.out.print(litr.next().getName()+" ");
 		}
 
 		litr = lunch.listIterator();
 		System.out.printf("%-15s","\nLunch: ");
 		while(litr.hasNext())
 		{
-			System.out.print(litr.next().getName()+" ");
+		//	System.out.print(litr.next().getName()+" ");
 		}
 
 		litr = snack0.listIterator();
 		System.out.printf("%-15s","\nSnack0: ");
 		while(litr.hasNext())
 		{
-			System.out.print(litr.next().getName()+" ");
+		//	System.out.print(litr.next().getName()+" ");
 		}
 		
 		litr = dinner.listIterator();
 		System.out.printf("%-15s","\nDinner: ");
 		while(litr.hasNext())
 		{
-			System.out.print(litr.next().getName()+" ");
+		//	System.out.print(litr.next().getName()+" ");
 		}
 
 		litr = snack1.listIterator();
 		System.out.printf("%-15s","\nSnack 1: ");
 		while(litr.hasNext())
 		{
-			System.out.print(litr.next().getName()+" ");
+		//	System.out.print(litr.next().getName()+" ");
 		}
 		System.out.println();		
 	}
@@ -293,7 +208,7 @@ public class NutriTrack extends Application
 		/*combobox containing list of database actions -- need event listener?*/
 		ComboBox<String> selectActionCB = new ComboBox<String>(); selectActionCB.setItems(actions);
 		/*all necessary labels*/
-		Label nameLabel = new Label("Name*: ");Label actionLabel = new Label("Select action: ");
+		Label nameLabel = new Label("Name**: ");Label actionLabel = new Label("Select action**: ");
 		Label servLabel = new Label("Serving Size: ");Label fatLabel = new Label("Fat: ");
 		Label cholLabel = new Label("Cholesterol: ");Label sodiumLabel = new Label("Sodium: ");
 		Label carbLabel = new Label("Carbohydrates: ");Label proteinLabel = new Label("Protein: ");
@@ -310,13 +225,17 @@ public class NutriTrack extends Application
 			@Override
 			public void handle(ActionEvent event) 
 			{
-				/*make sure action selected in combobox*/
-				if(selectActionCB.getValue()==null || nameField.getText().equals(""))statusLabel.setText("No action selected or name field empty.");
-				//else if()
+				/*make sure action selected in combobox and name filled*/
+				if(selectActionCB.getValue()==null || nameField.getText().equals(""))statusLabel.setText("A required field is empty.");
+				/*pass data to modifyFoodDB method*/
 				else 
 				{
 					statusLabel.setText("");
-					modifyFoodDB(selectActionCB.getValue(),nameField.getText(),servField.getText());
+					Food f = new Food(nameField.getText(),servField.getText(),fatField.getText(),cholField.getText(),sodiumField.getText(),
+							carbField.getText(),proteinField.getText());
+					String s = modifyFoodDB(selectActionCB.getValue(),nameField.getText(),servField.getText(),fatField.getText(),cholField.getText(),sodiumField.getText(),
+							carbField.getText(),proteinField.getText());
+					statusLabel.setText(s);
 				}
 			}
 		});
@@ -325,22 +244,36 @@ public class NutriTrack extends Application
 		return rightVBox;
 	}
 	
-	private void modifyFoodDB(String...info)
+	private String modifyFoodDB(String...arr)
 	{
-		Connection conn = null; Statement s = null; ResultSet rs = null;
+		Connection conn = null; Statement s = null; ResultSet rs; String m="";
 		/*try to connect to mysql DB*/
 		try
 		{
 			conn = DriverManager.getConnection("jdbc:mysql://localhost/nutri","root","password");
 			s = conn.createStatement();
-			rs = s.executeQuery("SELECT * FROM food");
+			switch(arr[0].toLowerCase())
+			{
+				case "add":
+					s.executeUpdate("INSERT INTO food(name,serv_size,fat,cholesterol,sodium,carbs,protein) "
+							+ "VALUES("+"'"+arr[1]+"'"+","+arr[2]+","+arr[3]+","+arr[4]+","+arr[5]+","+arr[6]+","+arr[7]+");");
+					m="Success";
+					break;
+				case "update":
+					break;
+				case "delete":
+					break;				
+			}
+			//rs = s.executeQuery("SELECT * FROM food");
 			//while(rs.next()!=false)
 				//System.out.println(rs.getString("name"));
-			conn.close();s.close();rs.close();
+			conn.close();s.close();
 		}
 		catch(Exception e)
 		{
+			m="Failed";
 			e.printStackTrace();
 		}
+		finally{return m;}
 	}
 }
