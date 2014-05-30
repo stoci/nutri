b170b93842ba36bdab56e16f63dce625ab1c314a
stoci <jablitz@ncsu.edu>
2014-05-29 22:29:29 -0400

    begun setting up left

--- src/com/main/NutriTrack.java
+++ src/com/main/NutriTrack.java
@@ -5,7 +5,9 @@ import src.com.day.*;
 import src.com.food.*;
 
 import java.sql.*;
+import java.text.SimpleDateFormat;
 import java.util.*;
+import java.util.Date;
 import java.io.*;
 
 import javafx.application.Application;
@@ -13,10 +15,13 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.FlowPane;
@@ -30,6 +35,9 @@ public class NutriTrack extends Application
 	static LinkedHashMap<String,Day> calendar_db = new LinkedHashMap<String,Day>();
 	// key = name while value is food object
 	static HashMap<String,Food> food_db = new HashMap<String,Food>();
+	/*database interaction objects*/
+	Connection conn = null; Statement s = null; ResultSet rs=null; 
+	ArrayList<String> result=new ArrayList<String>();
 	
 	static public void main(String [] args) throws Exception
 	{
@@ -186,28 +194,84 @@ public class NutriTrack extends Application
 		/*overall window layout: top,left,center,right,bottom*/
 		BorderPane borderPane = new BorderPane();
 		/*layout of left which is for adding food eaten to specific day->meal*/
-		VBox leftVBox= new VBox();
+		VBox leftVBox= configureLeft();
 		/*layout of right which is for RUD'ing food in the database*/
 		VBox rightBox = configureRight();
+		/*top has menubar*/
+		MenuBar menuBar = configureMenuBar();
 
 		
 		
 		
 		borderPane.setLeft(leftVBox); borderPane.setRight(rightBox);
+		borderPane.setTop(menuBar);
 		Scene scene = new Scene(borderPane, 1280, 720);
 		primaryStage.setScene(scene); primaryStage.setTitle("NutriTrack v0.1");
 		primaryStage.show();
 	}
 	
+	/*standard menubar configuration*/
+	private MenuBar configureMenuBar()
+	{
+		MenuBar menuBar = new MenuBar();
+		Menu menuFile = new Menu("File");
+		Menu menuAbout = new Menu("About");
+		menuBar.getMenus().addAll(menuFile,menuAbout);
+		return menuBar;
+	}
+	
+	/*allows adding food to meal*/
+	private VBox configureLeft()
+	{
+		/*set up one column layout*/
+		VBox leftVBox = new VBox(5);
+		modifyFoodDB("select name",null);
+		/*foods in database list*/
+		ObservableList<String> foods = FXCollections.observableArrayList(result);
+		/*combobox containing all food objects*/
+		ComboBox<String> nameCB = new ComboBox<String>(); nameCB.setItems(foods);
+		/*get today's date*/
+		Date date = new Date();
+		SimpleDateFormat sdf = new SimpleDateFormat("mm/dd/yyyy");
+
+
+		/*combobox containing types of meals -- breakfast, lunch, dinner, snack0, snack1*/
+		ObservableList<String> types = FXCollections.observableArrayList("Breakfast","Lunch","Snack0","Dinner","Snack1");
+		ComboBox<String> typeCB = new ComboBox<String>(); typeCB.setItems(types);
+		/*all necessary labels*/
+		Label titleLabel = new Label("Interact with meal table\n"); titleLabel.setStyle("-fx-font-size: 15px;");
+		Label nameLabel = new Label("Name**: ");Label dateLabel = new Label("Date**: ");
+		Label servLabel = new Label("Serving Size: ");Label typeLabel = new Label("Meal: ");
+		Label statusLabel = new Label(); statusLabel.setStyle("-fx-text-fill: #ff6347");
+		/*all necessary textfields*/
+		TextField dateField = new TextField(sdf.format(date));TextField servField = new TextField();
+		/*right submit button*/
+		Button leftBtn = new Button("Submit"); 
+		/*event listener*/
+		leftBtn.setOnAction(new EventHandler<ActionEvent>(){
+			@Override
+			public void handle(ActionEvent event) 
+			{
+				
+			}
+		});
+		leftVBox.setPadding(new Insets(0,10,0,10));
+		leftVBox.getChildren().addAll(titleLabel,dateLabel,dateField,typeLabel,typeCB,servLabel,
+				servField,nameLabel,nameCB,leftBtn,statusLabel);
+		return leftVBox;
+	}
+	
+	/*allows addition/update/remove Food from DB*/
 	private VBox configureRight()
 	{
-		/*set up two column layout*/
-		VBox rightVBox = new VBox(); //rightFPane.setPrefWrapLength(190); rightFPane.setVgap(2);rightFPane.setHgap(2);
+		/*set up one column layout*/
+		VBox rightVBox = new VBox(5); //rightFPane.setPrefWrapLength(190); rightFPane.setVgap(2);rightFPane.setHgap(2);
 		/*database actions list*/
 		ObservableList<String> actions = FXCollections.observableArrayList("Add","Update","Delete");
 		/*combobox containing list of database actions -- need event listener?*/
 		ComboBox<String> selectActionCB = new ComboBox<String>(); selectActionCB.setItems(actions);
 		/*all necessary labels*/
+		Label titleLabel = new Label("Interact with food table\n"); titleLabel.setStyle("-fx-font-size: 15px;");
 		Label nameLabel = new Label("Name**: ");Label actionLabel = new Label("Select action**: ");
 		Label servLabel = new Label("Serving Size: ");Label fatLabel = new Label("Fat: ");
 		Label cholLabel = new Label("Cholesterol: ");Label sodiumLabel = new Label("Sodium: ");
@@ -238,14 +302,15 @@ public class NutriTrack extends Application
 				}
 			}
 		});
-		rightVBox.getChildren().addAll(actionLabel, selectActionCB,nameLabel,nameField,servLabel,servField,fatLabel,fatField,
+		rightVBox.setPadding(new Insets(0,10,0,10));
+		rightVBox.getChildren().addAll(titleLabel,actionLabel, selectActionCB,nameLabel,nameField,servLabel,servField,fatLabel,fatField,
 				cholLabel,cholField,sodiumLabel,sodiumField,carbLabel,carbField,proteinLabel,proteinField,rightBtn,statusLabel);
 		return rightVBox;
 	}
 	
 	private String modifyFoodDB(String action, Food f)
 	{
-		Connection conn = null; Statement s = null; ResultSet rs; String m="";
+		String m="";
 		/*try to connect to mysql DB*/
 		try
 		{
@@ -257,16 +322,21 @@ public class NutriTrack extends Application
 					s.executeUpdate("INSERT INTO food(name,serv_size,fat,cholesterol,sodium,carbs,protein) "
 							+ "VALUES("+"'"+f.getName()+"'"+","+f.getServ_size()+","+f.getFat()+","+f.getCholesterol()
 							+","+f.getSodium()+","+f.getCarbs()+","+f.getProtein()+");");
-					m="Success";
-					break;
+					m="Success";modifyFoodDB("select name",null);break;
 				case "update":
 					break;
 				case "delete":
-					break;				
+					break;
+				case "select name":
+					rs = s.executeQuery("SELECT name FROM food;");
+					m="Success";break;
 			}
-			//rs = s.executeQuery("SELECT * FROM food");
-			//while(rs.next()!=false)
-				//System.out.println(rs.getString("name"));
+			/*rs = s.executeQuery("SELECT * FROM food");
+			while(rs.next()!=false)
+				System.out.println(rs.getString("name"));*/
+			while(rs.next()!=false)
+					result.add(rs.getString("name"));
+			
 			conn.close();s.close();
 		}
 		catch(Exception e)
